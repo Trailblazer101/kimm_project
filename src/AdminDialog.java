bd1dc8a864d7a2f089c3d2e0a9f4a55cf9cd97ab
Trailblazer101 <tthorntone@gmail.com>
2017-03-05 15:14:22 -0500

    Init
    First Commit.

---
+++ src/AdminDialog.java
@@ -0,0 +1,86 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class AdminDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			AdminDialog dialog = new AdminDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	Boolean showDialog() {
+		setModalityType(ModalityType.APPLICATION_MODAL);
+	    setVisible(true);
+	    
+	    return textField.getText().equals("admin");
+	}
+	/**
+	 * Create the dialog.
+	 */
+	public AdminDialog() {
+		setBounds(100, 100, 450, 168);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblPassword = new JLabel("Password:");
+			lblPassword.setBounds(51, 23, 75, 15);
+			contentPanel.add(lblPassword);
+		}
+		{
+			textField = new JTextField();
+			textField.setBounds(144, 23, 114, 19);
+			contentPanel.add(textField);
+			textField.setColumns(10);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						AdminDialog.this.setVisible(false);
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						textField.setText("");
+						AdminDialog.this.setVisible(false);
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
---
+++ src/LoginWindow.java
@@ -0,0 +1,121 @@
+import java.awt.Dialog.ModalityType;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+
+public class LoginWindow {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginWindow window = new LoginWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 * 
+	 */
+	public LoginWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(166, 65, 114, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(166, 143, 114, 19);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setLabelFor(textField);
+		lblUsername.setBounds(67, 41, 103, 15);
+		frame.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(67, 112, 103, 15);
+		frame.getContentPane().add(lblPassword);
+		
+		final JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+			}
+		});
+		btnNewButton.setBounds(180, 202, 81, 25);
+		frame.getContentPane().add(btnNewButton);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		
+	//Code Starts here
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+		
+		JMenuItem mntmAdmin = new JMenuItem("Admin");				// Checks to see if user is admin
+		mntmAdmin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				AdminDialog dialog = new AdminDialog();
+				
+				Boolean cool = dialog.showDialog();
+				
+				if(cool)
+				{
+					btnNewButton.setText("Start Server");
+				}
+			}
+			
+		});
+		mnHelp.add(mntmAdmin);
+	}
+	
+	public void test()
+	{
+		
+	}
+}
